06504634d896bc9c7a4e65cae6e54d334adc5c9f
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-12-03 17:58:20 -0500

    rearchitected js files and CLI based on tutor and TA sessions

--- index.js
+++ index.js
@@ -1,55 +1,257 @@
-// node requirements
+// functions to run the app
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { newManager, newEngineer, newIntern } = require('./questions');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const employeesArray = [];
+const createTeam = require('./src/generatePage');
 
-const managersArray = [];
-const engineersArray = [];
-const internsArray = [];
+// add a new manager and then ask what type of team member to add next
+const newManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is your name?',
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    return true;
+                } else {
+                    console.log('Name is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'What is your employee ID?',
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('Employee ID is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'emailAddress',
+            message: 'What is your email address?',
+            validate: emailAddressInput => {
+                if (emailAddressInput) {
+                    return true;
+                } else {
+                    console.log('Email address is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'phoneNumber',
+            message: 'What is your office phone number?',
+            validate: phoneNumberInput => {
+                if (phoneNumberInput) {
+                    return true;
+                } else {
+                    console.log('Phone number is required');
+                    return false;
+                }
+            }
+        },
+    ])
+        .then((data) => {
+            const managerName = data.managerName
+            const employeeId = data.employeeId
+            const emailAddress = data.emailAddress
+            const phoneNumber = data.phoneNumber
 
-const askEngineer = () => {
-    newEngineer().then(responses => {
-        const engineerCard = new Engineer(responses.engineerName, responses.employeeId, responses.engineerEmail, responses.engineerGitHubUserName);
+            const manager = new Manager(managerName, employeeId, emailAddress, phoneNumber)
 
-        // push to the eng array after collecting the responses
-        engineersArray.push(engineerCard);
+            employeesArray.push(manager);
 
-        nextStep(responses.newTeamMember);
-    })
+            // console.log(employeesArray);
+
+            nextStep()
+        })
 };
 
-const askIntern = () => {
-    newIntern().then(responses => {
-        const internCard = new Intern(responses.internName, responses.employeeId, responses.internGitHubUserName, responses.internSchool);
+// add a new engineer
+const newEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?",
+            validate: engineerNameInput => {
+                if (engineerNameInput) {
+                    return true;
+                } else {
+                    console.log('Name is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "employeeId",
+            message: "What is the engineer's employee ID?",
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('Employee ID is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email address?",
+            validate: engineerEmailInput => {
+                if (engineerEmailInput) {
+                    return true;
+                } else {
+                    console.log('Email address is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerGitHubUserName",
+            message: "What is the engineer's GitHub user name?",
+            validate: engineerGitHubUserNameInput => {
+                if (engineerGitHubUserNameInput) {
+                    return true;
+                } else {
+                    console.log('GitHub user name is required');
+                    return false;
+                }
+            }
+        },
+    ])
+        .then((data) => {
+            const engineerName = data.engineerName
+            const employeeId = data.employeeId
+            const engineerEmail = data.engineerEmail
+            const engineerGitHubUserName = data.engineerGitHubUserName
 
-        // push to the intern array after collecting the responses
-        internsArray.push(internCard);
+            const engineer = new Engineer(engineerName, employeeId, engineerEmail, engineerGitHubUserName)
 
-        nextStep(responses.newTeamMember);
-    })
+            employeesArray.push(engineer);
+
+            nextStep()
+        })
 };
 
-const nextStep = (choice) => {
-    if (choice === 'engineer') {
-        askEngineer();
-    }
-    else if (choice === 'intern') {
-        askIntern();
-    } else {
-        console.log(
-            managersArray, engineersArray, internsArray
+// add a new intern
+const newIntern = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?",
+            validate: internNameInput => {
+                if (internNameInput) {
+                    return true;
+                } else {
+                    console.log('Name is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "employeeId",
+            message: "What is the interns's employee ID?",
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('Employee ID is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internGitHubUserName",
+            message: "What is the intern's GitHub user name?",
+            validate: internGitHubUserNameInput => {
+                if (internGitHubUserNameInput) {
+                    return true;
+                } else {
+                    console.log('GitHub user name is required');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the intern's school?",
+            validate: internSchoolInput => {
+                if (internSchoolInput) {
+                    return true;
+                } else {
+                    console.log('School is required');
+                    return false;
+                }
+            }
+        },
+    ])
+        .then((data) => {
+            const internName = data.internName
+            const employeeId = data.employeeId
+            const internGitHubUserName = data.internGitHubUserName
+            const internSchool = data.internSchool
+
+            const intern = new Intern(internName, employeeId, internGitHubUserName, internSchool)
+
+            employeesArray.push(intern);
+            // console.log(internCard);
+
+            nextStep()
+        }
         )
-    }
 };
 
-// function to run run the app, starting by getting the manager info
-newManager().then(responses => {
-    const managerCard = new Manager(responses.managerName, responses.employeeId, responses.emailAddress, responses.phoneNumber);
+// ask what team member to add next
+const nextStep = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'newTeamMember',
+        message: 'Do you want to add a team member?',
+        choices: ['engineer', 'intern', 'done']
+    }).then(data => {
+        switch (data.newTeamMember) {
+            case 'engineer':
+                newEngineer();
+                break;
+            case 'intern':
+                newIntern();
+                break;
+            case 'done':
+                console.log('done');
+                generateTeam();
+                break;
+        }
+    })
+};
 
-    managersArray.push(managerCard);
+const generateTeam = () => {
+    fs.writeFile('./dist/index.html', createTeam(employeesArray), err => {
+        if (err) throw new Error(err);
+        console.log('File generated!');
+        // console.log(employeesArray);
+    })
+};
 
-    nextStep(responses.newTeamMember);
-});
+newManager();
+
+// module.exports = { newManager, newEngineer, newIntern };
--- src/generatePage.js
+++ src/generatePage.js
@@ -1,3 +1,95 @@
 // functions to create the page content by type (similar to unit 9)
 
-html assets
+const createTeam = (employeesArray) => {
+
+    const cardsArray = [];
+
+    const createManager = manager => {
+
+        return `
+            <div class="card mx-3 my-3" style="max-width: 360px" id="employee">
+                <div class="card-header">
+                    <h2>Manager: ${manager.name}</h2>
+                </div>
+                <div class="card-fill">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${manager.id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+                        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+                    </ul>
+                </div>
+            </div>
+            `
+    };
+
+    // create engineer card if needed
+    const createEngineer = engineer => {
+
+        return `
+            <div class="card mx-3 my-3" style="max-width: 360px" id="employee">
+                <div class="card-header">
+                    <h2>Engineer: ${engineer.name}</h2>
+                </div>
+                <div class="card-fill">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${engineer.id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+                    </ul>
+                </div>
+            </div>
+            `
+    }
+
+    // create intern card if needed
+    const createIntern = intern => {
+        return `
+            <div class="card mx-3 my-3" style="max-width: 360px" id="employee">
+                <div class="card-header">
+                    <h2>Intern: ${intern.name}</h2>
+                </div>
+                <div class="card-fill">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${intern.id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                        <li class="list-group-item">School: ${intern.school}</li>
+                    </ul>
+                </div>
+            </div>
+            `
+    };
+
+    cardsArray.push(employeesArray.filter(employee => employee.getRole() === "Manager").map(manager => createManager(manager)));
+    cardsArray.push(employeesArray.filter(employee => employee.getRole() === "Engineer").map(engineer => createEngineer(engineer)));
+    cardsArray.push(employeesArray.filter(employee => employee.getRole() === "Intern").map(intern => createIntern(intern)));
+    return cardsArray.join("");
+
+};
+
+module.exports = employeesArray => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, chrink-to-fit=no" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+                <link rel="stylesheet" href="./style.css" />
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+                <title>Team Overview</title>
+            </head>
+            <body>
+                <header>
+                    <div class="card-header">
+                        <h1>Team Overview</h1>
+                    </div>
+                </header>
+                <div id="team-container">
+                <div class = "d-flex flex-wrap justify-content-center">
+                    ${createTeam(employeesArray)}
+                </div>
+            </body>
+        </html>
+    `
+};
